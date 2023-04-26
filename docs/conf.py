b29e154fe07fa6ba4947024d6d9b6b0d3aa25567
Max Rossmannek <oss@zurich.ibm.com>
2023-04-26 15:42:41 -0400

    Update the intersphinx mapping URL for Qiskit Nature
    (cherry picked from commit 2a307eb82895b38a5c01bf78d0712e9bda50b774)

--- docs/conf.py
+++ docs/conf.py
@@ -210,7 +210,7 @@ intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable", None),
     "scipy": ("https://docs.scipy.org/doc/scipy", None),
-    "qiskit_nature": ("https://qiskit.org/documentation/nature", None),
+    "qiskit_nature": ("https://qiskit.org/ecosystem/nature", None),
     "qiskit": ("https://qiskit.org/documentation", None),
 }
 
